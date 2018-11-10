a9c3ce90fbb8c1aaaa1a9562f560fc064d3b50b0
Eduard Karpiel <eduard@karpiel.sk>
2018-11-11 00:45:11 +0100

    Removed redudant parts
    Signed-off-by: Eduard Karpiel <eduard@karpiel.sk>

--- test/classes/Navigation/NavigationHeaderTest.php
+++ test/classes/Navigation/NavigationHeaderTest.php
@@ -8,18 +8,8 @@
 
 namespace PhpMyAdmin\Tests\Navigation;
 
-use PhpMyAdmin\Config;
 use PhpMyAdmin\Navigation\NavigationHeader;
 use PhpMyAdmin\Tests\PmaTestCase;
-
-/*
- * we must set $GLOBALS['server'] here
- * since 'check_user_privileges.inc.php' will use it globally
- */
-$GLOBALS['server'] = 0;
-$GLOBALS['cfg']['Server']['DisableIS'] = false;
-
-require_once 'libraries/check_user_privileges.inc.php';
 
 /**
  * Tests for PhpMyAdmin\Navigation\NavigationHeader class
@@ -43,23 +33,16 @@ class NavigationHeaderTest extends PmaTestCase
     protected function setUp()
     {
         $GLOBALS['server'] = 1;
-        $GLOBALS['PMA_Config'] = new Config();
-        $GLOBALS['PMA_Config']->enableBc();
-        $GLOBALS['cfg']['Server']['host'] = 'localhost';
-        $GLOBALS['cfg']['Server']['user'] = 'root';
-        $GLOBALS['cfg']['Server']['pmadb'] = '';
+
+        $GLOBALS['cfg']['NavigationDisplayServers'] = true;
         $GLOBALS['cfg']['Server']['auth_type'] = 'http';
-        $GLOBALS['cfg']['Server']['DisableIS'] = false;
         $GLOBALS['cfg']['Servers'] = [];
-        $GLOBALS['cfg']['NavigationTreeEnableGrouping'] = true;
-        $GLOBALS['cfg']['ShowDatabasesNavigationAsTree'] = true;
 
         $GLOBALS['cfg']['NavigationLinkWithMainPanel'] = true;
         $GLOBALS['cfg']['NavigationTreePointerEnable'] = true;
         $GLOBALS['cfg']['NavigationTreePointerEnable'] = true;
 
         $GLOBALS['pmaThemeImage'] = 'image';
-        $GLOBALS['db'] = 'db';
 
         $this->object = new NavigationHeader();
     }
