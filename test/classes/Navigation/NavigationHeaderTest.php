412852247315660ef46091a8300c1f2d08c16558
Eduard Karpiel <eduard@karpiel.sk>
2018-11-11 14:18:12 +0100

    Added more tests for NavigationHeader
    Signed-off-by: Eduard Karpiel <eduard@karpiel.sk>

--- test/classes/Navigation/NavigationHeaderTest.php
+++ test/classes/Navigation/NavigationHeaderTest.php
@@ -78,4 +78,77 @@ class NavigationHeaderTest extends PmaTestCase
 
         $this->assertContains('<div id="pma_navigation_tree" class="list_container synced highlight autoexpand">', $result);
     }
+
+    /**
+     * Tests _logo() method.
+     *
+     * @return void
+     * @throws \ReflectionException
+     */
+    public function testLogo()
+    {
+        $GLOBALS['cfg']['NavigationDisplayLogo'] = false;
+
+        $method = $this->getMethod('_logo');
+
+        $this->assertEquals("", $method->invoke($this->object));
+
+        $GLOBALS['cfg']['NavigationDisplayLogo'] = true;
+        $GLOBALS['cfg']['NavigationLogoLink'] = '';
+
+        $this->assertEquals(
+            "    <div id=\"pmalogo\">\n"
+            . "                phpMyAdmin\n"
+            . "            </div>\n",
+            $method->invoke($this->object)
+        );
+
+        $GLOBALS['cfg']['NavigationLogoLink'] = 'index.php';
+        $GLOBALS['cfg']['NavigationLogoLinkWindow'] = 'new';
+
+        $this->assertContains(
+            htmlentities('target="_blank" rel="noopener noreferrer"'),
+            $method->invoke($this->object)
+        );
+    }
+
+    /**
+     * Tests _serverChoice() method.
+     *
+     * @return void
+     * @throws \ReflectionException
+     */
+    public function testServerChoice()
+    {
+        $GLOBALS['cfg']['Servers'] = [1, 2, 3];
+        $method = $this->getMethod('_serverChoice');
+
+        $this->assertEquals(
+            '<!-- SERVER CHOICE START -->'
+            . '<div id="serverChoice">'
+            . '<form method="post" action="index.php" class="disableAjax">'
+            . '<input type="hidden" name="token" value="token" />'
+            . '<label for="select_server">Current server:</label> '
+            . '<select name="server" id="select_server" class="autosubmit">'
+            . '<option value="">(Servers) ...</option>'
+            . "\n"
+            . '</select></form></div>'
+            . '<!-- SERVER CHOICE END -->',
+            $method->invoke($this->object)
+        );
+    }
+
+    /**
+     * Get inaccessible method (protected / private) and make it public using reflection.
+     *
+     * @param string $name Method name
+     * @return \ReflectionMethod
+     * @throws \ReflectionException
+     */
+    public function getMethod($name) {
+        $class = new \ReflectionClass('PhpMyAdmin\Navigation\NavigationHeader');
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
 }
